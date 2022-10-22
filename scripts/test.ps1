29fefc3a2a156b68d3460162db01ff7b545a01a2
benvr <vre19001@byui.edu>
2022-10-21 18:00:32 -0600

    10/21/2022

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,3 +1,14 @@
 ﻿function greet {
-    
+    if($args.count){
+        "Hello $args, how are you?"
+    }else{
+        'No one to say hello to.'
+    }
+}
+
+function q1{$args}
+
+function isodd($num){
+    if($num %2 -eq 1){return $true}
+    $false
 }
