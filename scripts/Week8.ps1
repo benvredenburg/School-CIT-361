37558d084cc3e51a978f8f1ecc74607fe5ed0c18
benvr <vre19001@byui.edu>
2022-10-31 19:03:25 -0600

    Week8.ps1 first commit

---
+++ scripts/Week8.ps1
@@ -0,0 +1,11 @@
+<#
+Program Name : Week8.ps1
+Date: 10/31/2022
+Author: Benjamin Vredenburg
+Corse: CIT361
+I, Benjamin Vredenburg, affirm that I wrote this script as original work completed by me.
+#> 
+
+# Clear screen for fresh run.
+Clear-Host
+
